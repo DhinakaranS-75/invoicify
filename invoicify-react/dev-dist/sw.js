2de849f2c9be7c39f8ee1e0444a9a7a567ef65c1
dhinakaran75493@gmail.com <dhinakaran75493@gmail.com>
2026-08-20 14:17:48 +0530

    update code

--- invoicify-react/dev-dist/sw.js
+++ invoicify-react/dev-dist/sw.js
@@ -81,7 +81,7 @@ define(['./workbox-afac4cd2'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.8j2utkioinc"
+    "revision": "0.14tvqdkhas"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
