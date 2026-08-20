72907fe1f3dc4a5464f1a9cf63a4e0a5179690c1
DhinakaranS-75 <dhinakaran75493@gmail.com>
2026-08-20 23:10:49 +0530

    website logo update

--- invoicify-react/dev-dist/sw.js
+++ invoicify-react/dev-dist/sw.js
@@ -81,7 +81,7 @@ define(['./workbox-afac4cd2'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.14tvqdkhas"
+    "revision": "0.d3588mitidc"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
