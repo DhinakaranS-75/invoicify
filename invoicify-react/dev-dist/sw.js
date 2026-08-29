25004b053c52105798f00adc7f2cf554fd986cf9
DhinakaranS-75 <dhinakaran75493@gmail.com>
2026-08-29 22:35:42 +0530

    update mail

--- invoicify-react/dev-dist/sw.js
+++ invoicify-react/dev-dist/sw.js
@@ -81,7 +81,7 @@ define(['./workbox-afac4cd2'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.47aa5ut5ttg"
+    "revision": "0.a7h6ilhh9cc"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
